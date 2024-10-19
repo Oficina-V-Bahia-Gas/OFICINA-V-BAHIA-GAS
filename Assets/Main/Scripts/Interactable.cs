8d0fec8af967f4f0cb19bf4c9abb0d172199e510
IgnysDev <ignysdev@gmail.com>
2024-10-19 19:49:47 -0300

    Criando o objeto de interação

---
+++ Assets/Main/Scripts/Interactable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private bool interact = false;
+    [SerializeField] private GameObject interactable_object;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            Interaction();
+        }
+    }
+
+    void Interaction()
+    {
+        if (interact)
+        {
+            Debug.Log("Interaction");
+            //interactable_object.
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        interact = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        interact = false;
+    }
+
+    
+}
