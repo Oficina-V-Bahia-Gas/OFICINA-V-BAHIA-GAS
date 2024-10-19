a70935891852c21ceb58fc94f8e2099ddfca8ddc
victorsilva21 <visipasa@gmail.com>
2024-10-19 19:48:09 -0300

    TextMeshPro baixado e timer programado

---
+++ Assets/Main/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.Mathematics;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float _levelTimer = 100;
+    [SerializeField] private TMP_Text _timerText;
+    private float _remainingTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _remainingTime = _levelTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimerDecrease();
+        TimerVisualization();
+    }
+    void TimerDecrease()
+    {
+        if (_remainingTime > 0)
+        {
+            _remainingTime -= Time.deltaTime;
+        }
+    }
+    void TimerVisualization()
+    {
+        float minutes;
+        float seconds;
+
+        minutes = Mathf.FloorToInt(_remainingTime / 60);
+        seconds = _remainingTime % 60;
+
+        _timerText.text = $"{minutes:00} : {seconds:00}";
+    }
+}
