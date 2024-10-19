067630d24c63febcdf5efc7716c907fd3ec02c8d
victorsilva21 <visipasa@gmail.com>
2024-10-19 20:48:49 -0300

    botão de retorno feito

--- Assets/Main/Scripts/LevelManager.cs
+++ Assets/Main/Scripts/LevelManager.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private Timer _timer;
-    [SerializeField] private SceneAsset _defeatScene;
+    [SerializeField] private Object _defeatScene;
     // Start is called before the first frame update
     void Start()
     {
