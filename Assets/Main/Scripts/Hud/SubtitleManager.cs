f15f70ec9575d79092adb1145d80d3b2efcbfd9c
Gustavo Dimas <gustavodimas.92@gmail.com>
2025-01-30 01:06:50 -0300

    add: Script de dublagem (Testar)

---
+++ Assets/Main/Scripts/Hud/SubtitleManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class SubtitleManager : MonoBehaviour
+{
+    [SerializeField] List<Subtitle> subtitles;
+    [SerializeField] float initialDelay = 1f;
+    [SerializeField] TextMeshProUGUI subtitleTxt;
+    [SerializeField] AudioSource subtitleAudio;
+
+    Coroutine subtitleCoroutine;
+
+    private void Start()
+    {
+        if (subtitleTxt != null)
+        {
+            subtitleTxt.text = "";
+        }
+    }
+
+    public void StartSubtitles()
+    {
+        if (subtitleCoroutine != null)
+        {
+            StopCoroutine(subtitleCoroutine);
+        }
+
+        subtitleCoroutine = StartCoroutine(PlayAllSubtitles());
+    }
+
+    public void StopSubtitles()
+    {
+        if (subtitleCoroutine != null)
+        {
+            StopCoroutine(subtitleCoroutine);
+            subtitleCoroutine = null;
+        }
+
+        if (subtitleTxt != null)
+        {
+            subtitleTxt.text = "";
+        }
+
+        if (subtitleAudio != null)
+        {
+            subtitleAudio.Stop();
+        }
+    }
+
+    IEnumerator PlayAllSubtitles()
+    {
+        yield return new WaitForSeconds(initialDelay);
+
+        foreach (Subtitle subtitle in subtitles)
+        {
+            if (subtitleTxt != null)
+            {
+                subtitleTxt.text = subtitle.text;
+            }
+
+            if (subtitleAudio != null && subtitle.voice != null)
+            {
+                subtitleAudio.PlayOneShot(subtitle.voice);
+            }
+
+            yield return new WaitForSeconds(subtitle.duration);
+        }
+
+        if (subtitleTxt != null)
+        {
+            subtitleTxt.text = "";
+        }
+    }
+}
+
+[Serializable]
+public class Subtitle
+{
+    public string text;
+    public float duration;
+    public AudioClip voice;
+}
