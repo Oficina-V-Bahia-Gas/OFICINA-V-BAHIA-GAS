1faabf760c7927257e4f3625e9a295ab3e9f5474
Iuri Guimaraes Guerra <iuriguimaraes@hotmail.com>
2025-02-06 22:45:25 -0300

    Menu / Creditos Atualizado

--- Assets/Iuri/HUD/Script DTW/BTN_Dtw.cs
+++ Assets/Iuri/HUD/Script DTW/BTN_Dtw.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BTN_Dtw : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Configurações da Pulsação")]
+    [SerializeField] private float pulseScale = 1.2f; // Escala máxima do pulso
+    [SerializeField] private float pulseDuration = 0.8f; // Duração de cada pulsação
+
+    private RectTransform _rectTransform; // Referência ao RectTransform do botão
+
     void Start()
     {
-        
+        // Garante que o componente RectTransform está atribuído
+        _rectTransform = GetComponent<RectTransform>();
+
+        if (_rectTransform != null)
+        {
+            StartPulseAnimation();
+        }
+        else
+        {
+            Debug.LogError("RectTransform não encontrado no objeto!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartPulseAnimation()
     {
-        
+        // Cria a animação de pulsação em loop
+        _rectTransform.DOScale(pulseScale, pulseDuration)
+            .SetEase(Ease.InOutSine) // Suaviza a transição
+            .SetLoops(-1, LoopType.Yoyo); // Loop infinito (Yoyo = vai e volta)
+    }
+
+    void OnDestroy()
+    {
+        // Interrompe a animação ao destruir o objeto
+        DOTween.Kill(_rectTransform);
     }
 }
